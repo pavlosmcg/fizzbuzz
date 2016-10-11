ba15c004e6558a9c026436b5229675f7e4fa9367
pavlosmcg <paul.mcgrath@comparethemarket.com>
2016-10-11 20:17:21 +0100

    Inital commit of FizzBuzz console app

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a start number: ");
+            string start = Console.ReadLine();
+            Console.Write("Please enter an end number: ");
+            string end = Console.ReadLine();
+            Console.WriteLine("And the results are:");
+            // TODO iterate through results here
+        }
+    }
+}
